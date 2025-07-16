0b08c72edc07780b2750e8a086fe783d2743b12e
SAHIBJOT <sahibjot.benipal@gmail.com>
2025-07-16 21:42:32 +0530

    hello food

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema =  mongoose.Schema({
    name:{ type : String , required : true },
    email:{ type : String , required : true , unique : true },
    imageUrl :{ type: String , required : true  },
-   carItems: { type : object , default: {} }
+   carItems: { type : Object , default: {} }
    
 } , {minimize:false})
 
